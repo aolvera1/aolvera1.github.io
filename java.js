782e06c9de76f219b3ac1aedfc39c1b04bdedc21
Alejandra Olvera <Alejandra.Olvera@icumed.com>
2020-09-17 07:21:42 -0600

    commit inicial

---
+++ java.js
@@ -0,0 +1,7 @@
+// function myFunction() {
+//     var x = document.java;
+//     document.getElementById("demo").innerHTML = x;
+//   }
+document.write("Last Updated: " + document.java +""); 
+
+// http://www.web-source.net/web_design_tips/last_modified_date_web_page_script.htm#.XqCiafl7lEY
